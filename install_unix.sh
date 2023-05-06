87277b1a33171bc55bb77398b653e583b6496061
Denis <denis.petrov@sharix-app.org>
2023-05-06 18:48:03 +0000

    update script v_0.3

--- install_unix.sh
+++ install_unix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git clone -b unstable http://git.sharix-app.org/ShariX_Open/sharix-open-tickets.git tickets
 git clone -b metasynced_module http://git.sharix-app.org/ShariX_Open/sharix-open-backend.git metaservicesynced
-python3 -m venv env
+python3 -m venv webapp-base
 source env/bin/activate
-pip3 install --updgrade pip
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 #python3 manage.py makemigrations
 #python3 manage.py migrate
