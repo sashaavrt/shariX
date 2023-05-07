d7c78e3be47df138d28acf65445d685ade4ed265
Evgeny Polivanov <polivanov004@gmail.com>
2023-05-07 15:00:15 +0000

    test update script

--- bin/webuser.sh
+++ bin/webuser.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/sharix-open-webapp-base
-exec /root/sharix-open-webapp-base/webapp-base/bin/gunicorn core.wsgi:application -c core/conf_gunicorn.py
+exec /root/sharix-open-webapp-base/env/bin/gunicorn core.wsgi:application -c core/conf_gunicorn.py
--- install_unix.sh
+++ install_unix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone -b unstable ssh://gogs@git.sharix-app.org/ShariX_Open/sharix-open-tickets.git tickets
 git clone -b metasynced_module ssh://gogs@git.sharix-app.org/ShariX_Open/sharix-open-backend.git metaservicesynced
-python3 -m venv webapp-base
-source webapp-base/bin/activate
+python3 -m venv env
+source env/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
