4e8a1159fe0eeab90930c2f5ea93b82a9d942df5
Denis <denis.petrov@sharix-app.org>
2023-05-25 21:34:19 +0000

    fix install_unix

--- install_unix.sh
+++ install_unix.sh
@@ -3,6 +3,7 @@ git clone -b unstable https://git.sharix-app.org/ShariX_Open/sharix-open-tickets
 git clone -b metasynced_module https://git.sharix-app.org/ShariX_Open/sharix-open-backend.git metaservicesynced
 git clone -b webinterface https://git.sharix-app.org/ShariX_Open/sharix-open-webadmin.git SharixAdmin/templates/SharixAdmin
 git clone -b master https://git.sharix-app.org/ShariX_Open/sharix-open-local.git openlocal
+git clone -b master https://git.sharix-app.org/ShariX_Open/sharix-open-config.git conf
 python3 -m venv env
 source env/bin/activate
 pip3 install --upgrade pip
@@ -12,3 +13,4 @@ python3 manage.py migrate
 python3 manage.py collectstatic
 python3 manage.py createsuperuser
 deactivate
+cp conf/sharix_open.service /etc/systemd/system/
