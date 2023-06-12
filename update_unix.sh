72966e577a7739acd6993dbc12ad0f3e12ce899c
Староверов Данила Андреевич <staroverovdanila@gmail.com>
2023-06-12 11:22:25 +0300

    add update scripts

---
+++ update_unix.sh
@@ -0,0 +1,13 @@
+git pull
+cd design_template/
+git pull
+cd ..
+cd tickets/ 
+git pull
+cd ..
+cd metaservicesynced/
+git pull
+cd ..
+cd openlocal/
+git pull
+cd ..
---
+++ update_win.bat
@@ -0,0 +1,6 @@
+@echo off
+git pull
+cd design_template && git pull && cd ..
+cd tickets && git pull && cd ..
+cd metaservicesynced && git pull && cd ..
+cd openlocal && git pull && cd ..
