993d2895546c5b0990520799f86c3449a2e5b39f
TonyKurts <tonykurts@yandex.ru>
2023-06-19 17:55:50 +0000

    update script DN

--- install_unix.sh
+++ install_unix.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-cd design_template/
-git clone https://git.sharix-app.org/ShariX_Open/sharix-open-webapp-design-template.git
+git clone https://git.sharix-app.org/ShariX_Open/sharix-open-webapp-design-template.git design_template
+cd design_template
 git checkout unstable
 cd ..
-cd tickets/
-git clone https://git.sharix-app.org/ShariX_Open/sharix-open-tickets.git 
+git clone https://git.sharix-app.org/ShariX_Open/sharix-open-tickets.git tickets 
+cd tickets
 git checkout unstable
 git cd ..
-cd metaservicesynced/
-git clone https://git.sharix-app.org/ShariX_Open/sharix-open-backend.git
+git clone https://git.sharix-app.org/ShariX_Open/sharix-open-backend.git metaservicesynced
+cd metaservicesynced
 git checkout unstable
 cd ..
-cd sharix-open-config/
-git clone https://git.sharix-app.org/ShariX_Open/sharix-open-config.git
+git clone https://git.sharix-app.org/ShariX_Open/sharix-open-config.git sharix-open-config
+cd sharix-open-config
 git checkout unstable
 cd ..
-cd webservice_running/
-git clone https://git.sharix-app.org/ShariX_Open/sharix-open-webservice-running.git
+git clone https://git.sharix-app.org/ShariX_Open/sharix-open-webservice-running.git webservice_running
+cd webservice_running
 git checkout unstable
 cd .. 
 python3 -m venv env
@@ -28,4 +28,4 @@ python3 manage.py migrate
 python3 manage.py collectstatic
 python3 manage.py createsuperuser
 deactivate
-cp conf/sharix_open.service /etc/systemd/system/
+cp sharix-open-config/sharix_open.service /etc/systemd/system/
