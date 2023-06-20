7e11a2e5492db9b8fab2a3ea9272c5a8a260e798
1Mike21 <mihail.skosyrev@yandex.ru>
2023-06-20 18:53:55 +0300

    Updated logo

--- core/jazzmin_settings.py
+++ core/jazzmin_settings.py
@@ -9,7 +9,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "ShariX Platform",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "SharixAdmin/img/logo.svg",
+    "site_logo": "SharixAdmin/img/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
