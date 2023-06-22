77e489d218a5b95b39a783c27e8790b40f3b861f
1Mike21 <mihail.skosyrev@yandex.ru>
2023-06-22 12:04:23 +0000

    updated page authorization

--- bin/webuser.sh
+++ bin/webuser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #cd /path/to/project
-cd /root/sharix-open-webapp-base
+cd /root/sharix-open-webapp-base/
 #exec /path/to/project/env/bin/gunicorn core.wsgi:application -c core/conf_gunicorn.py
 exec /root/sharix-open-webapp-base/env/bin/gunicorn core.wsgi:application -c core/conf_gunicorn.py
