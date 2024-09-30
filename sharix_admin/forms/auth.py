e262d1694b7173003fc628397344cfc641d59562
TonyKurts <tonykurts@yandex.ru>
2024-09-30 17:45:31 +0300

    Clarification of the registration process
    - Added an "I am 14 years old" checkbox.
    - The username now automatically matches the phone number (temporary stopper).
    - Added small hints to the fields.

--- sharix_admin/forms/auth.py
+++ sharix_admin/forms/auth.py
@@ -7,11 +7,22 @@ from sharix_admin.forms import BaseForm
 
 class ShariXSignUpForm(BaseForm, UserCreationForm):
     """
-    Форма для регистрации пользователей.
+    Форма для регистрации пользователей. 
     """
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['phone_number'] # FIXME: Имя пользователя = номер телефона
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = get_user_model()
-        fields = ('last_name', 'first_name', 'middle_name', 'phone_number', 'email', 'username')
+        fields = ('last_name', 'first_name', 'middle_name', 'phone_number', 'email')
+        labels = {
+            'middle_name': 'Отчество (не обязательно)',
+            'phone_number': 'Номер телефона (только цифры)',
+        }
 
 
 class ShariXLoginForm(BaseForm, AuthenticationForm):
