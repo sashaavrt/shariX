1beb0df030b792512102fcca5d1f636313cd901b
blezz-tech <markus.jenya04@yandex.ru>
2025-02-12 10:11:55 +0300

    Revert "fix"
    This reverts commit 20e7ea1cadd81d7a5944f50fcf8a71a43ba29ab2.

--- sharix_admin/forms/service.py
+++ sharix_admin/forms/service.py
@@ -17,8 +17,8 @@ class ServiceTariffUpdateForm(forms.ModelForm):
         fields = [
             'status',
             'ticket_status',
-            'servicetype',
-            'resource',
+            'servicetype_id',
+            'resource_id',
             'requirements',
             'price_alg',
             'price_km',
@@ -32,8 +32,8 @@ class ServiceTariffUpdateForm(forms.ModelForm):
             'status': forms.TextInput(attrs={'readonly': True}, ),
             'ticket_status': forms.TextInput(attrs={'readonly': True}),
             
-            'servicetype': forms.Select(attrs={'class': 'form-select'}),
-            'resource': forms.Select(attrs={'class': 'form-select'}),
+            'servicetype_id': forms.Select(attrs={'class': 'form-select'}),
+            'resource_id': forms.Select(attrs={'class': 'form-select'}),
         }
 
 
