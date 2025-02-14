5087638ca952a56fa5282b7d600eabdddaad1862
blezz-tech <markus.jenya04@yandex.ru>
2025-02-14 10:43:07 +0000

    fix filed resource_id in models

--- sharix_admin/tables.py
+++ sharix_admin/tables.py
@@ -304,7 +304,7 @@ class ServiceTariffTable(tables.Table):
             "class": "table table-layout-fixed"
         }
         exclude = (
-            'resource_id',
+            'resource',
             'price_alg',
             'price_min',
             'price_amount',
@@ -442,7 +442,7 @@ class ServiceTable(tables.Table):
             "class": "table table-layout-fixed"
         }
         exclude = (
-            'resource_id',
+            'resource',
             'requirements',
             'id_metaservice',
             'price_alg',
